f04205988d8bf666953b5121ff2dd3d3b6cec83f
Avisa Poshtkouhi <aposhtkouhi@gmail.com>
2020-07-10 19:30:45 -0700

    Add comment in to file to explain why I use glob

--- PILScriptsPython.py
+++ PILScriptsPython.py
@@ -4,7 +4,8 @@ import os, glob
 from PIL import Image
 
 newsize = 128, 128
-
+# because we do not know what's in the file we use glob
 for file in glob.glob("ic_*"):
+       # open the file and convert to RGB
        im = Image.open(file).convert('RGB')
        im.rotate(270).resize((newsize)).save("/opt/icons/" + file, "JPEG")
